72ae5bc19570ed12509e8d730cca2bf24d0f3e21
omiya0555 <otoharadak@gmail.com>
2024-10-07 22:52:01 +0900

    Change the contentHeight 700->600.

--- src/components/CalendarEvent.vue
+++ src/components/CalendarEvent.vue
@@ -20,7 +20,7 @@
                         center: "title",
                         end: "next"
                     },
-                    contentHeight: 700,
+                    contentHeight: 600,
                     events: [
                     { title: 'event 1', date: '2024-10-01' },
                     { title: 'event 2', date: '2024-10-02' },
